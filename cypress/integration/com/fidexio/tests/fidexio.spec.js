7d111050031c3f62d0bd89a7f8c479d02055e88f
Adrk51 <durakahmet049@gmail.com>
2022-08-14 22:38:42 +0200

    Faker

--- cypress/integration/com/fidexio/tests/fidexio.spec.js
+++ cypress/integration/com/fidexio/tests/fidexio.spec.js
@@ -3,6 +3,7 @@
 import calendarPage from '../pages/calendarPage';
 import loginPage from '../pages/loginPage';
 import surveyPage from '../pages/surveyPage';
+import { faker } from '@faker-js/faker';
 
 describe('As a POSManager, I should be able to create and desgin a new survey from Survey module',()=>{
     let login = new loginPage();
@@ -11,9 +12,10 @@ describe('As a POSManager, I should be able to create and desgin a new survey fr
         cy.viewport(1500,1000);
         login.loginFidexio();
         cy.get('.oe_topbar_name').contains('POSManager10').should('be.visible');
-    })
-    it.only('As a POSManager, user verify all buttons before creating a Survey',()=>{
         login.selectMenu("surveys");
+    })
+    it.skip('As a POSManager, user verify all buttons before creating a Survey',()=>{
+        
         cy.url().should('include','survey')
         
         //click Create button
@@ -24,11 +26,48 @@ describe('As a POSManager, I should be able to create and desgin a new survey fr
         survey.listView();
         survey.kanbanView();
     })
+})
 
-    it('As a Posmanager, I should be able to create and to see my meetings and events on my calendar from "Calendar" module',async()=>{
-        const calendar = new calendarPage();
-        login.selectMenu('Calendar');
+//const faker = require("faker");
+describe('Verify all buttons work after creating a Survey',{defaultCommandTimeout:4000},()=>{
+    it('User create a Survey',()=>{
+        // click 'create' button
+        cy.get('button.o-kanban-button-new').click({force:true});
+        cy.title().should('eq','New - Odoo')
+    })
 
-        calendar.monthPicker("Sep");
+    it('User click Design Survey button',()=>{
+        const surveyName = faker.animal.dog();
+        cy.log(surveyName);
+        cy.get("input[placeholder='Survey Title']").type(surveyName);
+        // click Design Survey button 
+        cy.get(".o_statusbar_buttons > :first-child").wait(1000).click()
+    })
+    it.skip('User click Start Survey button',()=>{
+
+    })
+    it.skip('User click Back to Survey button',()=>{
+        
+    })
+    it.skip('User click Test Survey button',()=>{
+        
+    })
+    it.skip('User click Test Survey button',()=>{
+        
+    })
+    it.skip('User click Back to Survey button',()=>{
+        
+    })
+    it.skip('User click Print Survey button',()=>{
+        
+    })
+    it.skip('User click Back to Survey button',()=>{
+        
+    })
+    it.skip('User click Print Share and invite by email',()=>{
+        
+    })
+    it.skip('User click View results Survey button',()=>{
+        
     })
 })
