69a8db439d612352fa45777a21a70fa330567051
Adrk51 <durakahmet049@gmail.com>
2022-08-17 23:00:31 +0200

    update

--- cypress/integration/com/fidexio/tests/Fleet.spec.js
+++ cypress/integration/com/fidexio/tests/Fleet.spec.js
@@ -7,23 +7,27 @@ const login = new loginPage();
 const fleet = new fleetPage();
 
 
-describe('Pos manager can create new Vehicle Model',{defaultCommandTimeout:5000},()=>{
+describe('Pos manager can create new Vehicle Model',()=>{
     before('As a POSManager, user is on the home page',()=>{
         login.loginFidexio();
         cy.get('.oe_topbar_name').contains('POSManager10').should('be.visible');
     })
-    it('User navigate to Fleet page',()=>{
+    it('User navigates to Fleet page',()=>{
         login.selectMenu('Fleet');
         cy.url().should('include','fleet')
         
     })
-    it(' Users hits the Vehicle Model',()=>{
+    it('User hits the Vehicle Model',()=>{
         fleet.leftSideMenuSelection("Vehicle Model");
         fleet.createBtn();
+    })
+    it('User enters the Model name',()=>{
         fleet.enterModelName();
         fleet.selectMaker("Alfa");
         cy.wait(5000);
         fleet.saveBtn();
+    })
+    it('User deletes the created Model',()=>{
         cy.wait(5000);
         fleet.actions("Delete")
     })
