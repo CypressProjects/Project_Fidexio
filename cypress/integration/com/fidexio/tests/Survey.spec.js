779c9884c0d0fabfd50087ec9ecc0473283e943e
Adrk51 <durakahmet049@gmail.com>
2022-08-17 22:18:57 +0200

    survey structure is corrected

--- cypress/integration/com/fidexio/tests/Survey.spec.js
+++ cypress/integration/com/fidexio/tests/Survey.spec.js
@@ -6,45 +6,48 @@ import surveyPage from '../pages/surveyPage';
 const login = new loginPage();
 const survey = new surveyPage();
 
-describe('As a POSManager, I should be able to create and design a new survey from Survey module',{retries:2},()=>{
+describe('As a POSManager, I should be able to create and design a new survey from Survey module',()=>{
     before('As a POSManager, user is on the home page',()=>{
         login.loginFidexio();
         cy.get('.oe_topbar_name').contains('POSManager10').should('be.visible');
     })
-    it.skip('As a POSManager, user verify all buttons before creating a Survey',()=>{
-        login.selectMenu("surveys");
-        cy.url().should('include','survey')
-        
-        survey.createBtn();
-        survey.discardBtn();
-        survey.importBtn();
-        survey.cancelBtn();
-        survey.listView();
-        survey.kanbanView();
+    describe('As a POSManager, user verify all buttons before creating a Survey',()=>{
+        it('',()=>{
+            login.selectMenu("surveys");
+            cy.url().should('include','survey')
+
+            survey.createBtn();
+            survey.discardBtn();
+            survey.importBtn();
+            survey.cancelBtn();
+            survey.listView();
+            survey.kanbanView();
+        })
+    })
+
+    describe('Verify all buttons work after creating a Survey',()=>{
+        it('User create a Survey',()=>{
+            login.selectMenu("surveys");
+            cy.url().should('include','survey')
+    
+            //survey.createSurvey();
+            //survey.isSurveyCreated(survey.surveyName);
+            survey.isSurveyCreated("Pink salmon")
+            survey.threeDotDelete("Pink salmon");
+        })
+    
+        it('User verify all buttons working properly',()=>{
+            survey.gotoSurvey("Pink salmon");  // <---  survey.surveyName   replace it later
+    
+            survey.designSurveyBtn();
+            cy.go('back');
+            survey.testSurvey();
+            cy.go(-1);
+    
+            
+        })
     })
 })
 
 
-//const faker = require("faker");
-describe('Verify all buttons work after creating a Survey',{defaultCommandTimeout:8000},()=>{
-    it('User create a Survey',()=>{
-        login.selectMenu("surveys");
-        cy.url().should('include','survey')
 
-        //survey.createSurvey();
-        //survey.isSurveyCreated(survey.surveyName);
-        survey.isSurveyCreated("Pink salmon")
-        survey.threeDotDelete("Pink salmon");
-    })
-
-    it.skip('User verify all buttons working properly',()=>{
-        survey.gotoSurvey("Pink salmon");  // <---  survey.surveyName   replace it later
-
-        survey.designSurveyBtn();
-        cy.go('back');
-        survey.testSurvey();
-        cy.go(-1);
-
-        
-    })
-})
