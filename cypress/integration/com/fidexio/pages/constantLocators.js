aea317dd342030d13fcb05bd32ab7c681601dace
Adrk51 <durakahmet049@gmail.com>
2022-08-29 14:42:01 +0200

    demo constant locator test

--- cypress/integration/com/fidexio/pages/constantLocators.js
+++
@@ -1,9 +0,0 @@
-/// <reference types='Cypress' />
-
-class constantLocators{
-    HOMEPAGE = {
-        DEMOS : "div#menu li:nth-child(2) > a"
-    }
-
-}
-export default constantLocators;
--- cypress/integration/com/fidexio/tests/getLocaterTest.spec.js
+++ cypress/integration/com/fidexio/tests/getLocaterTest.spec.js
@@ -1,6 +1,6 @@
 /// <reference types='Cypress' /> 
 
-import constantLocators from "../pages/constantLocators";
+import constantLocators from "../ConstantLocators/demoLocators";
 
 const locaters = new constantLocators();
 
@@ -9,5 +9,13 @@ describe('',()=>{
         cy.visit('https://fineuploader.com/');
 
         cy.get(locaters.HOMEPAGE.DEMOS).click();
+        cy.get(locaters.HOMEPAGE.DEMOS_SIDE_MENU.BASIC_SETUP).click();
+        cy.wait(1000).go('back');
+        cy.get(locaters.HOMEPAGE.DEMOS_SIDE_MENU.GALLERY_VIEW).click();
+        cy.wait(1000).go('back');
+        cy.get(locaters.HOMEPAGE.DEMOS_SIDE_MENU.MANUAL_TRIGGER).click();
+        cy.wait(1000).go('back');
+        cy.get(locaters.HOMEPAGE.DEMOS_SIDE_MENU.UPLOAD_AMAZON).click();
+        cy.wait(1000).go('back');
     })
 })
